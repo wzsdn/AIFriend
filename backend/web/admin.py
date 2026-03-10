f119021272f2c07b6a0573ca4c17d9c750229c21
wzsdn <341698541@qq.com>
2026-03-10 18:41:48 +0800

    实现朋友界面流式布局和朋友交流弹窗界面前后端

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from web.models.user import UserProfile
 from web.models.character import Character
-
+from web.models.friend import Friend
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ('user',)
@@ -10,3 +10,7 @@ class UserProfileAdmin(admin.ModelAdmin):
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me','character',)
